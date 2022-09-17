33f2bcdbfc13d91ddf5fe2f078cc218d4303d02a
Ash Luchowa <ashwinluchowa@gmail.com>
2022-09-17 21:36:47 +0800

    Create initial board + buttons

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const container = document.getElementById('container');
+let board = document.querySelector('.board');
+board.style.gridTemplateColumns = 'repeat(16, 1fr)';
+board.style.gridTemplateRows = 'repeat(16, 1fr)';
+board.style.border = '1px solid #000000';
+
+for (let i = 0; i < 256; i++) {
+    let square = document.createElement('div');
+    square.style.border = '1px solid #000000';
+    board.insertAdjacentElement('beforeend', square);
+}
