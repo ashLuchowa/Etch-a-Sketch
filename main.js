b2833481bc55e096acd427d8c0d2679db71db2d6
Ash Luchowa <ashwinluchowa@gmail.com>
2022-09-17 16:42:39 +0800

    Create grid Functions

--- main.js
+++ main.js
@@ -1,3 +1,25 @@
 const container = document.getElementById('container');
 let rows = document.getElementsByClassName('gridRow');
 let cells = document.getElementsByClassName('cell');
+
+//Create a default 16x16 grid
+
+//Create Rows
+function makeRows(rowNum) {
+
+    for (r = 0; r < rowNum; r++) {
+        let row = document.createElement('div');
+        container.appendChild(row).className = 'gridRow';
+    }
+}
+
+//Create Columns
+function makeColumn(cellNum) {
+
+    for (i = 0; i < rows.length; i++) {
+        for (j = 0; j < cellNum; j++) {
+            let newCell = document.createElement('div');
+            rows[j].appendChild(newCell).className = 'cell';
+        }
+    }
+}
