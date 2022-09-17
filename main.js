1ba272a40c77b5d78757251a11eeaa9881f82fe0
Ash Luchowa <ashwinluchowa@gmail.com>
2022-09-17 22:33:17 +0800

    Build sketch event

--- main.js
+++ main.js
@@ -1,10 +1,17 @@
 let board = document.querySelector('.board');
+
+//Create Grid
 board.style.gridTemplateColumns = 'repeat(16, 1fr)';
 board.style.gridTemplateRows = 'repeat(16, 1fr)';
 board.style.border = '1px solid #000000';
 
 for (let i = 0; i < 256; i++) {
-    let square = document.createElement('div');
+    const square = document.createElement('div');
     square.style.border = '1px solid #000000';
+
+    square.addEventListener('mouseover', ()=> {
+        square.style.backgroundColor = 'black';
+    });
+
     board.insertAdjacentElement('beforeend', square);
 }
