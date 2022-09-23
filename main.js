2f149feb8627265cc96bad751022c21ad6c185c0
Ash Luchowa <ashwinluchowa@gmail.com>
2022-09-23 15:11:46 +0800

    Working on GridSize

--- main.js
+++ main.js
@@ -1,59 +1,60 @@
 const board = document.querySelector('.board');
 const buttons = document.querySelectorAll('button');
+const setSizeButton = document.querySelector('#set-size-btn');
+const setValue = document.querySelector('#set-size-value');
 
 
-//Create Grid
-const gridSize = (gridColumns, gridRows) => {
-    board.style.gridTemplateColumns = `repeat(${gridColumns}, 1fr)`;
-    board.style.gridTemplateRows = `repeat(${gridRows}, 1fr)`;
-    board.style.border = '5px solid #000000';
+    //Default Grid
+    const gridSize = (gridColumns, gridRows) => {
+        board.style.gridTemplateColumns = `repeat(${gridColumns}, 1fr)`;
+        board.style.gridTemplateRows = `repeat(${gridRows}, 1fr)`;
+        board.style.border = '5px solid #000000';
 
-    for (let i = 0; i < `${gridColumns*gridRows}`; i++) {
-        const square = document.createElement('div');
-        square.style.border = '1px solid #000000';
+        for (let i = 0; i < `${gridColumns*gridRows}`; i++) {
+            const square = document.createElement('div');
+            square.style.border = '1px solid #000000';
 
 
-        buttons.forEach(() => {
-            //Choose Black
-            buttons[0].addEventListener('click', () => {
-                square.addEventListener('mouseover', () => {
-                    square.style.backgroundColor = 'black';
-                });
-            })
+            buttons.forEach(() => {
+                //Choose Black
+                buttons[0].addEventListener('click', () => {
+                    square.addEventListener('mouseover', () => {
+                        square.style.backgroundColor = 'black';
+                    });
+                })
 
-            //Choose White
-            buttons[1].addEventListener('click', () => {
-                square.addEventListener('mouseover', () => {
+                //Choose White
+                buttons[1].addEventListener('click', () => {
+                    square.addEventListener('mouseover', () => {
+                        square.style.backgroundColor = '';
+                    });
+                })
+
+                //Choose Rainbow
+                buttons[2].addEventListener('click', () => {
+                    square.addEventListener('mouseover', () => {
+
+                        //Random Color function
+                        function randomColor() {
+                            let x = Math.floor(Math.random() * 256);
+                            let y = Math.floor(Math.random() * 256);
+                            let z = Math.floor(Math.random() * 256);
+                            const bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
+                            square.style.backgroundColor = bgColor;
+                        }
+                        randomColor();
+                    });
+                })
+
+                //Choose Reset
+                buttons[3].addEventListener('click', () => {
                     square.style.backgroundColor = '';
                 });
+
             })
 
-            //Choose Rainbow
-            buttons[2].addEventListener('click', () => {
-                square.addEventListener('mouseover', () => {
-
-                    //Random Color function
-                    function randomColor() {
-                        let x = Math.floor(Math.random() * 256);
-                        let y = Math.floor(Math.random() * 256);
-                        let z = Math.floor(Math.random() * 256);
-                        const bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
-                        square.style.backgroundColor = bgColor;
-                    }
-                    randomColor();
-                });
-            })
-
-            //Choose Reset
-            buttons[3].addEventListener('click', () => {
-                square.style.backgroundColor = '';
-            });
-        })
-
-        board.insertAdjacentElement('beforeend', square);
+            board.insertAdjacentElement('beforeend', square);
+        }
     }
 
-}
-
-//Grid Size Variable
-gridSize(16, 16);
+gridSize(16,16);
