7dd15176b60904a8a82935d850d7ea36927d63cc
Ash Luchowa <ashwinluchowa@gmail.com>
2022-09-25 19:06:50 +0800

    Simplify Buttons

--- main.js
+++ main.js
@@ -1,4 +1,9 @@
+let color = 'black';
 const buttons = document.querySelectorAll('button');
+const blackBtn = document.getElementById('color-black');
+const colorRemoveBtn = document.getElementById('color-remove');
+const colorRainbowBtn = document.getElementById('color-rainbow');
+const colorResetBtn = document.getElementById('color-reset');
 const setSizeButton = document.querySelector('#set-size-btn');
 
 
@@ -11,59 +16,46 @@ function gridSize(setSize) {
     board.style.gridTemplateRows = `repeat(${setSize}, 1fr)`;
     board.style.border = '5px solid #000000';
 
-    let amount = setSize * setSize;
-    for (let i = 0; i < amount; i++) {
+    for (let i = 0; i < setSize * setSize; i++) {
         let square = document.createElement('div');
         square.style.border = '1px solid #000000';
-
-
-        //Coloring Function
-        buttons.forEach(() => {
-            //Choose Black
-            buttons[0].addEventListener('click', () => {
-                square.addEventListener('mouseover', () => {
-                    square.style.backgroundColor = 'black';
-                });
-            })
-
-            //Choose White
-            buttons[1].addEventListener('click', () => {
-                square.addEventListener('mouseover', () => {
-                    square.style.backgroundColor = '';
-                });
-            })
-
-            //Choose Rainbow
-            buttons[2].addEventListener('click', () => {
-                square.addEventListener('mouseover', () => {
-
-                    //Random Color function
-                    function randomColor() {
-                        let x = Math.floor(Math.random() * 256);
-                        let y = Math.floor(Math.random() * 256);
-                        let z = Math.floor(Math.random() * 256);
-                        const bgColor = 'rgb(' + x + ',' + y + ',' + z + ')';
-                        square.style.backgroundColor = bgColor;
-                    }
-                    randomColor();
-                });
-            })
-
-            //Choose Reset
-            buttons[3].addEventListener('click', () => {
-                square.style.backgroundColor = '';
-            });
-
-        })
-
+        square.addEventListener('mouseover', colorSquare);
         board.insertAdjacentElement('beforeend', square);
+
+        //Reset Btn
+        colorResetBtn.addEventListener('click', () => {
+            square.style.backgroundColor = '';
+        });
     }
 }
 
+//Default GridSize
+gridSize(16);
+
+//Change Grid Size Btn
 setSizeButton.addEventListener('click', () => {
     let sizePrompt = prompt("Type your grid size", "1-100");
     gridSize(sizePrompt);
-    console.log(sizePrompt);
 })
 
-gridSize(16);
+//colorSquare Function
+function colorSquare() {
+    this.style.backgroundColor = color;
+}
+
+//Buttons
+//Choose Black
+blackBtn.addEventListener('click', () => {
+    color = 'black';
+});
+//Choose Eraser
+colorRemoveBtn.addEventListener('click', () => {
+    color = '';
+});
+//Choose Rainbow
+colorRainbowBtn.addEventListener('click', () => {
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    color = 'rgb(' + x + ',' + y + ',' + z + ')';
+});
